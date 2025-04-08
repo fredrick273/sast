35c88b86714315bee1a635008fc6f8386519bb0f
Fredrick273 <samfredrick273@gmail.com>
2025-04-09 02:31:19 +0530

    bug fix #5

--- scanner/admin.py
+++ scanner/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import ScanList
+from .models import ScanList,Reports
 
 # Register your models here.
 admin.site.register(ScanList)
+admin.site.register(Reports)
