cf7cdb7a160f42c74a936dd4696964a0bebc3e1e
Fredrick273 <samfredrick273@gmail.com>
2025-04-09 03:20:07 +0530

    Bug Fix #8

--- backend/urls.py
+++ backend/urls.py
@@ -22,4 +22,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('',include('scanner.urls'))
-] + + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
